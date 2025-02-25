7e32d8387bfa72d1ad6679b51efa8fc5f7ad86de
ChaoukiBayoudhi <chaoukibayoudhi@gmail.com>
2025-02-25 13:21:24 +0100

    First commit

--- course_app/models.py
+++ course_app/models.py
@@ -40,3 +40,35 @@ class Tutor(models.Model):
     class Meta:
         db_table='tutors'
         ordering=['name','familyName']
+
+
+class Student(models.Model):
+    cin=models.CharField(max_length=8,primary_key=True)
+    name=models.CharField(max_length=100)
+    familyName=models.CharField(max_length=100)
+    email=models.EmailField(unique=True)
+    birthDate=models.DateField()
+    class Meta:
+        db_table="students"
+        ordering=["name","familyName"] #sort by name in ascending order, then by familyName in ascending order
+
+class Profile(models.Model):
+    linkedIn=models.URLField()
+    github=models.URLField()
+    photo=models.ImageField(upload_to="images/student_images/",null=True, blank=True)
+     class Meta:
+        db_table="profiles"
+        ordering=["linkedIn","github"]
+class Location(models.Model):
+    locationNumber=models.CharField(max_length=10)
+    streetName=models.CharField(max_length=100)
+    city=models.CharField(max_length=100)
+    zipCode=models.CharField(max_length=10)
+
+    class Meta:
+        db_table="locations"
+        constraints=[models.UniqueConstraint(
+            fields=["locationNumber","streetName","zipCode"],
+            name="unique_location"
+        )]
+#
